65fe501cfad91e8b290f0842e533a7c3b708a8be
DrakeCullen <dpcullen@mavs.coloradomesa.edu>
2023-04-26 19:47:00 -0600

    do it

--- client.py
+++ client.py
@@ -1,17 +1,16 @@
 # Import socket module
-import socket			
+import socket
 
 # Create a socket object
-s = socket.socket()		
+s = socket.socket()
 
 # Define the port on which you want to connect
-port = 12345			
+port = 12345
 
 # connect to the server on local computer
-s.connect(('127.0.0.1', port))
+s.connect(("192.168.122.1", port))
 
 # receive data from the server and decoding to get the string.
-print (s.recv(1024).decode())
+print(s.recv(1024).decode())
 # close the connection
-s.close()	
-	
+s.close()
