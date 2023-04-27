b2ec85c681a1f535fb60dfa6c4eff10d4f55e317
DrakeCullen <dpcullen@mavs.coloradomesa.edu>
2023-04-26 19:25:02 -0600

    do it

---
+++ game.py
@@ -0,0 +1,18 @@
+import pygame
+
+from pygame.locals import *
+
+pygame.init()
+
+WIDTH = 500
+HEIGHT = 500
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+
+    pygame.display.update()
