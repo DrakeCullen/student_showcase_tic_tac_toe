b3d9ea898f523239d5ca023a9ef2b7a0a2a5aa1b
DrakeCullen <dpcullen@mavs.coloradomesa.edu>
2023-05-02 20:36:21 -0600

    ya

--- server.py
+++ server.py
@@ -36,7 +36,7 @@ class server_boy:
         self.c.send((str(num[0]) + "," + str(num[1])).encode())
 
     def awaitMove(self):
-        tuple_thing = self.s.recv(1024).decode().split(",")
+        tuple_thing = self.c.recv(1024).decode().split(",")
         return [int(tuple_thing[0]), int(tuple_thing[1])]
 
     def close(self):
