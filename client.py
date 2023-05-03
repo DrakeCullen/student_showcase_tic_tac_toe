6ebb741017dfe1807889d04e95d0d2d016c98272
DrakeCullen <dpcullen@mavs.coloradomesa.edu>
2023-05-02 20:29:24 -0600

    ya

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ class socky_boy:
         self.s = socket.socket()
 
         # Define the port on which you want to connect
-        port = 12345
+        port = 12343
         # self.s.setblocking(0)
         # connect to the server on local computer
         self.s.connect(("10.5.108.240", port))
